786efe12109a93e8040a7d0e448e2e6a89c2616f
anthrofax <afridhoikhsan@gmail.com>
2024-08-05 08:20:15 +0700

    suspense

--- src/app/(pages)/destinations/page.tsx
+++ src/app/(pages)/destinations/page.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import Input from "@/ui/Input";
 import Button from "@/ui/Button";
 import { useDestinationHook } from "@/hooks/destination-hooks";
@@ -14,11 +14,9 @@ import toast from "react-hot-toast";
 import { DestinationFilterType } from "./type";
 import { Pagination } from "@/components/ui/pagination";
 import ImageSlider from "@/components/slider/slider";
-import { GetServerSideProps } from "next";
 
-const Destinations = ({ initialDestinations }: any) => {
-  const { allDestinations = initialDestinations, isLoading } =
-    useDestinationHook();
+const Destinations = () => {
+  const { allDestinations, isLoading } = useDestinationHook();
   const router = useRouter();
   const searchParams = useSearchParams();
   const lokasi = searchParams.get("lokasi");
@@ -122,62 +120,84 @@ const Destinations = ({ initialDestinations }: any) => {
   ];
 
   return (
-    <div className="w-full">
-      <ImageSlider images={sliderImages} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="w-full">
+        <ImageSlider images={sliderImages} />
 
-      <div className="relative z-20 -mt-12 h-full w-full flex flex-col items-center">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="border w-full md:w-2/3 p-4 md:px-4 py-6 rounded-xl bg-blue-600 text-slate-500 flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0"
-          >
-            <FormField
-              control={form.control}
-              name="lokasi"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      placeholder="Cari berdasarkan lokasi"
-                      className="text-blue-800 p-2 rounded-xl outline-none w-full"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+        <div className="relative z-20 -mt-12 h-full w-full flex flex-col items-center">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="border w-full md:w-2/3 p-4 md:px-4 py-6 rounded-xl bg-blue-600 text-slate-500 flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0"
+            >
+              <FormField
+                control={form.control}
+                name="lokasi"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Cari berdasarkan lokasi"
+                        className="text-blue-800 p-2 rounded-xl outline-none w-full"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <div className="flex flex-col items-center gap-1">
-              <h3 className="ml-1 text-[#efefef] font-semibold">Harga</h3>
-              <div className="flex flex-col md:flex-row items-center gap-2">
+              <div className="flex flex-col items-center gap-1">
+                <h3 className="ml-1 text-[#efefef] font-semibold">Harga</h3>
+                <div className="flex flex-col md:flex-row items-center gap-2">
+                  <FormField
+                    control={form.control}
+                    name="minHarga"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            min={0}
+                            placeholder="Harga min"
+                            className="text-blue-800 p-2 rounded-xl outline-none w-full"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="maxHarga"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="number"
+                            min={0}
+                            placeholder="Harga maks"
+                            className="text-blue-800 p-2 rounded-xl outline-none w-full"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center gap-1">
+                <h3 className="ml-1 text-[#efefef] font-semibold">Destinasi</h3>
                 <FormField
                   control={form.control}
-                  name="minHarga"
+                  name="destinasi"
                   render={({ field }) => (
                     <FormItem>
                       <FormControl>
                         <Input
-                          type="number"
-                          min={0}
-                          placeholder="Harga min"
-                          className="text-blue-800 p-2 rounded-xl outline-none w-full"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="maxHarga"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input
-                          type="number"
-                          min={0}
-                          placeholder="Harga maks"
+                          type="text"
+                          placeholder="Cari berdasarkan nama"
                           className="text-blue-800 p-2 rounded-xl outline-none w-full"
                           {...field}
                         />
@@ -186,72 +206,41 @@ const Destinations = ({ initialDestinations }: any) => {
                   )}
                 />
               </div>
-            </div>
-
-            <div className="flex flex-col items-center gap-1">
-              <h3 className="ml-1 text-[#efefef] font-semibold">Destinasi</h3>
-              <FormField
-                control={form.control}
-                name="destinasi"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        type="text"
-                        placeholder="Cari berdasarkan nama"
-                        className="text-blue-800 p-2 rounded-xl outline-none w-full"
-                        {...field}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
+              <Button
+                // disabled={isLoading}
+                label="Cari"
+                type="submit"
+                className="px-6 py-2 text-[20px] bg-white text-blue-600 rounded-xl transition-all hover:bg-[#efefef]"
               />
-            </div>
-            <Button
-              // disabled={isLoading}
-              label="Cari"
-              type="submit"
-              className="px-6 py-2 text-[20px] bg-white text-blue-600 rounded-xl transition-all hover:bg-[#efefef]"
+            </form>
+          </Form>
+          <div className="w-full flex flex-wrap justify-center items-center gap-14 flex-grow-1 py-20">
+            {isLoading
+              ? Array.from({ length: 5 }).map((_, idx) => (
+                  <Skeleton
+                    key={idx}
+                    className="rounded-xl shadow-md "
+                    height={500}
+                    width={350}
+                  />
+                ))
+              : paginatedDestinations.map(
+                  (destination: Destination, idx: number) => (
+                    <Card key={idx} destination={destination} />
+                  )
+                )}
+          </div>
+          {filteredDestinations.length > itemsPerPage && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={handlePageChange}
             />
-          </form>
-        </Form>
-        <div className="w-full flex flex-wrap justify-center items-center gap-14 flex-grow-1 py-20">
-          {isLoading
-            ? Array.from({ length: 5 }).map((_, idx) => (
-                <Skeleton
-                  key={idx}
-                  className="rounded-xl shadow-md "
-                  height={500}
-                  width={350}
-                />
-              ))
-            : paginatedDestinations.map(
-                (destination: Destination, idx: number) => (
-                  <Card key={idx} destination={destination} />
-                )
-              )}
+          )}
         </div>
-        {filteredDestinations.length > itemsPerPage && (
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={handlePageChange}
-          />
-        )}
       </div>
-    </div>
+    </Suspense>
   );
 };
 
 export default Destinations;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // Fetch data from your API or database here
-  const initialDestinations: any[] = []; // Replace with your data fetching logic
-
-  return {
-    props: {
-      initialDestinations,
-    },
-  };
-};
