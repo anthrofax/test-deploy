cf542864613011ede1d32c30f8ee4e99fc11773a
anthrofax <afridhoikhsan@gmail.com>
2024-08-05 09:42:50 +0700

    bottom useeffect

--- src/app/(pages)/destinations/[destinationId]/page.tsx
+++ src/app/(pages)/destinations/[destinationId]/page.tsx
@@ -54,54 +54,54 @@ function DestinationDetails() {
     defaultValue: 1,
   });
 
-  // async function handlePayment(data: FieldValues) {
-  //   try {
-  //     if (!dataDestinasi) return toast.error("Data destinasi tidak ditemukan");
+  async function handlePayment(data: FieldValues) {
+    try {
+      if (!dataDestinasi) return toast.error("Data destinasi tidak ditemukan");
 
-  //     if (Number(data.qty) > 2 && data.penginapan === "sikembang")
-  //       return toast.error(
-  //         "Penginapan di Sikembang Glamping hanya dapat menampung maksimum 2 orang"
-  //       );
+      if (Number(data.qty) > 2 && data.penginapan === "sikembang")
+        return toast.error(
+          "Penginapan di Sikembang Glamping hanya dapat menampung maksimum 2 orang"
+        );
 
-  //     toast.success("Data berhasil terkirim");
-  //     console.log(data);
-  //     let totalBiaya = 0;
+      toast.success("Data berhasil terkirim");
+      console.log(data);
+      let totalBiaya = 0;
 
-  //     const biayaExperience = experience.reduce((acc, experienceItem) => {
-  //       if (data.experience.includes(experienceItem.value))
-  //         return acc + experienceItem.harga;
+      const biayaExperience = experience.reduce((acc, experienceItem) => {
+        if (data.experience.includes(experienceItem.value))
+          return acc + experienceItem.harga;
 
-  //       return 0;
-  //     }, 0);
+        return 0;
+      }, 0);
 
-  //     const biayaPenginapan =
-  //       penginapan.find(
-  //         (penginapanItem) => penginapanItem.value === data.penginapan
-  //       )?.harga || 0;
+      const biayaPenginapan =
+        penginapan.find(
+          (penginapanItem) => penginapanItem.value === data.penginapan
+        )?.harga || 0;
 
-  //     totalBiaya =
-  //       dataDestinasi.price * Number(data.qty) +
-  //       biayaExperience +
-  //       biayaPenginapan * Number(data.masaPerjalanan);
+      totalBiaya =
+        dataDestinasi.price * Number(data.qty) +
+        biayaExperience +
+        biayaPenginapan * Number(data.masaPerjalanan);
 
-  //     await redirectToCheckout({
-  //       destinationId: dataDestinasi.destinationId,
-  //       experience: data.experience,
-  //       hargaDestinasi: dataDestinasi.price,
-  //       lokasiPenjemputan: data.lokasiPenjemputan,
-  //       masaPerjalanan: data.masaPerjalanan,
-  //       nama: data.nama,
-  //       namaDestinasi: dataDestinasi.destinationName,
-  //       nomorHp: data.nomorHp,
-  //       penginapan: data.penginapan,
-  //       qty: data.qty,
-  //       tanggalPerjalanan: data.tanggalPerjalanan,
-  //       totalBiaya,
-  //     });
-  //   } catch (err) {
-  //     toast.error("Pembayaran gagal");
-  //   }
-  // }
+      await redirectToCheckout({
+        destinationId: dataDestinasi.destinationId,
+        experience: data.experience,
+        hargaDestinasi: dataDestinasi.price,
+        lokasiPenjemputan: data.lokasiPenjemputan,
+        masaPerjalanan: data.masaPerjalanan,
+        nama: data.nama,
+        namaDestinasi: dataDestinasi.destinationName,
+        nomorHp: data.nomorHp,
+        penginapan: data.penginapan,
+        qty: data.qty,
+        tanggalPerjalanan: data.tanggalPerjalanan,
+        totalBiaya,
+      });
+    } catch (err) {
+      toast.error("Pembayaran gagal");
+    }
+  }
 
   useEffect(() => {
     if (Object.keys(form.formState.errors).length > 0) {
@@ -117,21 +117,21 @@ function DestinationDetails() {
     }
   }, [form.formState.errors, form]);
 
-  // useEffect(() => {
-  //   const snapScript = "https://app.sandbox.midtrans.com/snap/snap.js";
-  //   const clientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT as string;
+  useEffect(() => {
+    const snapScript = "https://app.sandbox.midtrans.com/snap/snap.js";
+    const clientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT as string;
 
-  //   const script = document.createElement("script");
-  //   script.src = snapScript;
-  //   script.setAttribute("data-client-key", clientKey);
-  //   script.async = true;
+    const script = document.createElement("script");
+    script.src = snapScript;
+    script.setAttribute("data-client-key", clientKey);
+    script.async = true;
 
-  //   document.body.appendChild(script);
+    document.body.appendChild(script);
 
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   // useEffect(() => {
   //   if (masaPerjalanan == 1) {
