1092ff224f727d7fb18336115d1df1f13d4b6234
anthrofax <afridhoikhsan@gmail.com>
2024-08-05 09:39:50 +0700

    pay

--- src/app/(pages)/destinations/[destinationId]/page.tsx
+++ src/app/(pages)/destinations/[destinationId]/page.tsx
@@ -117,21 +117,21 @@ function DestinationDetails() {
     }
   }, [form.formState.errors, form]);
 
-  useEffect(() => {
-    const snapScript = "https://app.sandbox.midtrans.com/snap/snap.js";
-    const clientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT as string;
+  // useEffect(() => {
+  //   const snapScript = "https://app.sandbox.midtrans.com/snap/snap.js";
+  //   const clientKey = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT as string;
 
-    const script = document.createElement("script");
-    script.src = snapScript;
-    script.setAttribute("data-client-key", clientKey);
-    script.async = true;
+  //   const script = document.createElement("script");
+  //   script.src = snapScript;
+  //   script.setAttribute("data-client-key", clientKey);
+  //   script.async = true;
 
-    document.body.appendChild(script);
+  //   document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
 
   useEffect(() => {
     if (masaPerjalanan == 1) {
